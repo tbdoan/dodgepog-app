9b3615d066d5e4086b7da51a813786ac05ae35f2
tbdoan <tbdoan@ucsd.edu>
2020-08-05 22:17:40 -0700

    looks decent

--- src/render.js
+++ src/render.js
@@ -1,13 +1,13 @@
-$(function(){
+$(function () {
   //uncomment this later
   //$('#players').hide();
   //remove following line later
   $('#waiting').remove();
-  for(let i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     let template = generateTemplate(i);
     $('#your-team').append(template);
   }
-  for(let i = 6; i <= 10; i++) {
+  for (let i = 6; i <= 10; i++) {
     let template = generateTemplate(i);
     $('#enemy-team').append(template);
   }
@@ -16,41 +16,64 @@ $(function(){
 const generateTemplate = (i) => {
   let template;
 
-  if(i<=5) {
-    template =
-      '<article class="media">' +
-        '<figure class="media-left">' +
-          '<p class="image is-32x32">' +
-            `<img src="https://bulma.io/images/placeholders/128x128.png" id="tile${i}">` +
-          '</p>' +
-        '</figure>' +
-        '<div class="media-content center">' +
-          '<div class="content is-small">' +
-            `<p id="champ${i}" class="bold-text">` +
-              `Champion ${i}` +
-            '</p>' +
-          '</div>' +
-        '</div>' +
-      '</article>'
+  if (i <= 5) {
+    template = `
+    <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <article class="media">
+            <figure class="media-left">
+              <img class="tile" id="tile${i}" src="https://bulma.io/images/placeholders/128x128.png">
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p id="champ${i}">
+                  Champion ${i}
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="flip-card-back">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                  <p>data</p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div> `
   }
 
 
   else {
-    template =
-      '<article class="media">' +
-        '<div class="media-content center">' +
-          '<div class="content is-small">' +
-            `<p class="enemy-champs bold-text" id="champ${i}">` +
-              `Champion ${i}` +
-            '</p>' +
-          '</div>' +
-        '</div>' +
-        '<figure class="media-right">' +
-          '<p class="image is-32x32">' +
-            `<img src="https://bulma.io/images/placeholders/128x128.png" id="tile${i}">` +
-          '</p>' +
-        '</figure>' +
-      '</article>'
+    template = `
+    <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <article class="media">
+          <figure class="media-left">
+              <img class="tile" id="tile${i}" src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p id="champ${i}">
+                Champion ${i}
+              </p>
+            </div>
+        </article>
+      </div>
+      <div class="flip-card-back">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+                <p>data</p>
+            </div>
+        </article>
+      </div>
+    </div> `
   }
 
   template.id = `slot${i}`;
@@ -58,36 +81,36 @@ const generateTemplate = (i) => {
 }
 
 async function start() {
-    let {PythonShell} = require('python-shell');
+  let { PythonShell } = require('python-shell');
 
-    let options = {
-        mode: 'text',
-        pythonOptions: ['-u'], // get print results in real-time
-        scriptPath: './scripts',
-      };
+  let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: './scripts',
+  };
 
-      let pyshell = new PythonShell('getplayers.py', options);
+  let pyshell = new PythonShell('getplayers.py', options);
 
-      pyshell.on('message', function (message) {
-        // received a message sent from the Python script (a simple 'print' statement)
-        const data = JSON.parse(message);
-        console.log(data);
-        $('#waiting').remove();
-        $('#players').show();
-        data['champ_names'].map((name, index) => {
-          if(name !== null) {
-              $(`#champ${index+1}`).text(name);
-              name = name.replace(/[^A-Za-z0-9]/g, '');
-              $(`#tile${index+1}`).attr('src', `assets/champ_tiles/${name}_0.jpg`)
-          }
-        })
-      });
+  pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple 'print' statement)
+    const data = JSON.parse(message);
+    console.log(data);
+    $('#waiting').remove();
+    $('#players').show();
+    data['champ_names'].map((name, index) => {
+      if (name !== null) {
+        $(`#champ${index + 1}`).text(name);
+        name = name.replace(/[^A-Za-z0-9]/g, '');
+        $(`#tile${index + 1}`).attr('src', `assets/champ_tiles/${name}_0.jpg`)
+      }
+    })
+  });
 
-      // end the input stream and allow the process to exit
-      pyshell.end(function (err,code,signal) {
-        if (err) throw err;
-        console.log('The exit code was: ' + code);
-        console.log('The exit signal was: ' + signal);
-        console.log('finished');
-      });
+  // end the input stream and allow the process to exit
+  pyshell.end(function (err, code, signal) {
+    if (err) throw err;
+    console.log('The exit code was: ' + code);
+    console.log('The exit signal was: ' + signal);
+    console.log('finished');
+  });
 }
